df001e9c4daadcd2418ccdc51ac84ee9a63f02d8
Erick Agrazal <erick.agrazal@gmail.com>
2018-05-31 21:05:28 -0500

    Added base for nodejs class

---
+++ NodeJsTests/config.js
@@ -0,0 +1,10 @@
+App = {
+    // Atritubos
+    // Métodos - Inicialización
+    init: function(){
+        console.log('Iniciando...');
+    },
+    // Métodos - Handlers
+}
+
+module.exports.ExportedApp = App;
---
+++ NodeJsTests/main.js
@@ -0,0 +1,13 @@
+// Ejemplo #1
+function diHolaA(nombre){
+    console.log('Hola: ' + nombre);
+}
+
+// diHolaA('Erick');
+
+// Ejemplo #2
+// console.log(module);
+
+// Ejemplo #3
+var App = require('./config');
+console.log(App.ExportedApp.init());
