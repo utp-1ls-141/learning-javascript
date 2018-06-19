3c0b9da27eebcb7b396fa29c0964b100340b59ed
Erick Agrazal <erick.agrazal@gmail.com>
2018-06-19 09:24:12 -0500

    Added bower and initial controllers

--- ExpressJsTests/index.js
+++ ExpressJsTests/index.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
-// Rutas
+// Setting View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Routes
 app.get('/', function(req, res){
-	
+	res.render('index');
 });
+
+app.get('/greet/:name', function(req, res){
+	res.render('greetAPerson', {
+		name: req.params.name,
+	});
+});
+
+// Open listening port
+// Set PORT:
+// Mac / Linux: export NODE_JS_PORT=3000
+const port = process.env.NODE_JS_PORT || 3000;
+app.listen(port, function(){ console.log(`Escuchando en el puerto ${port}...`) });
