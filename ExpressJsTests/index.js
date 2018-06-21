5ebda201601af6db11f5f4770e11e12ba75f1617
Erick Agrazal <erick.agrazal@gmail.com>
2018-06-21 09:15:56 -0500

    Added adminlte

--- ExpressJsTests/index.js
+++ ExpressJsTests/index.js
@@ -6,6 +6,10 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
+app.use(express.static(path.join(__dirname, 'bower_components')))
+app.use(express.static(path.join(__dirname, 'public')))
+
 // Routes
 app.get('/', function(req, res){
 	res.render('index');
